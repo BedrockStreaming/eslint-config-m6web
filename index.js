5402a8d67d7147a0f32e18967980ba011484077e
Florent Lepretre <florent.lepretre@gmail.com>
2018-03-29 17:17:16 +0200

    lint(rule): add no-negated-condition rule
    Add no-negated-condition rule in `error`
    See https://eslint.org/docs/rules/no-negated-condition

--- index.js
+++ index.js
@@ -59,6 +59,9 @@ module.exports = {
 
     // TODO: remove these rules when next version of Airbnb config will be released
     "react/prefer-stateless-function": ["error", {"ignorePureComponents": true}],
-    "react/no-unused-prop-types": ["error", {"skipShapeProps": true}]
+    "react/no-unused-prop-types": ["error", {"skipShapeProps": true}],
+    
+    // No if(!foo) {} else {} and things like this
+    "no-negated-condition": "error"
   }
 };
